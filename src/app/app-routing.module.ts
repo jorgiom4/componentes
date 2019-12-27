585102737370660285c37a09502f0bf016030a89
Gorka López-Quintana <glopezquintana@indra.es>
2019-12-27 14:53:35 +0100

    Comenzamos a implementar las paginas de navegacion y cambiamos la pagina de incio.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', 
-    redirectTo: 'home', 
-    pathMatch: 'full' 
+  { path: '', redirectTo: 'inicio', pathMatch: 'full'  },
+  { path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule) },
+  {
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
   },
   {
-    path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
 ];
 
