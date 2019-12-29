0d913ebe05f1d34889ab13620d42f5ff5a1acf27
Gorka López-Quintana <gorkalqy@hotmail.com>
2019-12-29 13:59:52 +0100

    Componente Alert
    Implementamos el componente de las alertas con las diversas opciones que nos da ionic, también implementamos un ejemplo un poco mas avanzado con la recepción de un input que viene del alert y el texto del input lo mostramos en el header de la página.

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -8,51 +8,50 @@ import {ActionSheetController} from '@ionic/angular';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor( public actionSheetController: ActionSheetController) {}
-
-    async presentActionSheet() {
-      const actionSheet = await this.actionSheetController.create({
-        backdropDismiss: false,
-        header: 'Albums',
-        buttons: [{
-          text: 'Delete',
-          role: 'destructive',
-          icon: 'trash',
-          cssClass: 'rojo',
-          handler: () => {
-            console.log('Delete clicked');
-          }
-        }, {
-          text: 'Share',
-          icon: 'share',
-          handler: () => {
-            console.log('Share clicked');
-          }
-        }, {
-          text: 'Play (open modal)',
-          icon: 'arrow-dropright-circle',
-          handler: () => {
-            console.log('Play clicked');
-          }
-        }, {
-          text: 'Favorite',
-          icon: 'heart',
-          handler: () => {
-            console.log('Favorite clicked');
-          }
-        }, {
-          text: 'Cancel',
-          icon: 'close',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        }]
-      });
-      await actionSheet.present();
-    }
+  constructor(public actionSheetController: ActionSheetController) {}
 
   ngOnInit() {
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      backdropDismiss: false,
+      header: 'Albums',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        cssClass: 'rojo',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'arrow-dropright-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
