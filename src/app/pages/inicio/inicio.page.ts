99983fd87a078735640a5fc26b5f97ccc1888bed
Gorka López-Quintana <gorkalqy@hotmail.com>
2020-01-01 12:19:29 +0100

    Componente Input y Forms
    Implementamos el componente ion-input y su uso en los formularios, sus validaciones en la aproximación por template.

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -63,6 +63,11 @@ export class InicioPage implements OnInit {
       icon: 'infinite',
       name: 'Infinite Scroll',
       redirectTo: '/infinite-scroll'
+    },
+    {
+      icon: 'hammer',
+      name: 'Input y Forms',
+      redirectTo: '/input'
     }
   ];
 
