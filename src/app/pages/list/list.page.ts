2728813f517330aa656a65d963090acbae9d1e22
Gorka López-Quintana <gorkalqy@hotmail.com>
2020-01-02 21:13:41 +0100

    Funcionalidad List Sliding
    Implementamos las posibilidades del ion-list-sliding con botones y comportamientos según la opción escogida.

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {DataService} from '../../services/data.service';
 import {Observable} from 'rxjs';
+import {IonList} from "@ionic/angular";
+import {share} from "rxjs/operators";
 
 @Component({
   selector: 'app-list',
@@ -8,6 +10,8 @@ import {Observable} from 'rxjs';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+
+  @ViewChild('lista', {static: true}) lista: IonList;
 
   usuarios: Observable<any>;
 
@@ -17,4 +21,18 @@ export class ListPage implements OnInit {
     this.usuarios = this.dataService.getUsers();
   }
 
+  favorite(user) {
+    console.log('Favorito:', user);
+    this.lista.closeSlidingItems();
+  }
+
+  share(user) {
+    console.log('Share:', user);
+    this.lista.closeSlidingItems();
+  }
+
+  borrar(user) {
+    console.log('Borrar:', user);
+    this.lista.closeSlidingItems();
+  }
 }
