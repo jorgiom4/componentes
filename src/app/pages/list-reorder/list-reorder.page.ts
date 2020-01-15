4841b0ec126b9fc252a75c0c6d9661e313404f08
Gorka López-Quintana <glopezquintana@indra.es>
2020-01-15 09:51:47 +0100

    Terminamos de implementar el componente de reordenación de listas ion-list-reorder

--- src/app/pages/list-reorder/list-reorder.page.ts
+++ src/app/pages/list-reorder/list-reorder.page.ts
@@ -14,4 +14,21 @@ export class ListReorderPage implements OnInit {
   ngOnInit() {
   }
 
+  reorder(event) {
+    // console.log(event);
+    // Obtenemos el item que se ha movido y lo quitamos de la lista
+    const itemMover = this.personajes.splice(event.detail.from, 1)[0];
+    console.log('item a mover: ' + itemMover);
+
+    // Insertamos el item en su posición
+    this.personajes.splice(event.detail.to, 0, itemMover);
+
+
+    event.detail.complete();
+  }
+
+  // Guardamos los cambios o hacemos algo con la lista final
+  guardar() {
+    console.log(this.personajes);
+  }
 }
